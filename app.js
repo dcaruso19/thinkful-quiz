1ca94f467dd38ba3040896c3ba05fa1a28e826e3
dcaruso19 <dcaruso19@gmail.com>
2015-01-05 02:27:14 -0500

    updated javascript in quiz to highlight right answers in green, wrong answer in red, and a functioning scorebox.

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ $(document).ready(function () {
     $(".reset").click(function() {
     location.reload(true);
 });
+    
     $(".start").click(trivia);
 
     function trivia() {
+
+        $(".scorebox").show();
+
         var scoreAry = [];
         var questions = [{
             q: "Who was the lead singer of the Doors?",
@@ -32,7 +36,7 @@ $(document).ready(function () {
             a: "They were naked",
             correct: 0
         }, {
-            q: "What country are Led Zeppelin from?",
+            q: "What country is Led Zeppelin from?",
             s: ["England", "Ireland", "Germany", "Australia"],
             a: "England",
             correct: 0
@@ -58,13 +62,15 @@ $(document).ready(function () {
             correct: 0
         }];
 
+
+       
         var counter = questions.length;
 
         //This grabs the question and answer data from the questions array and appends it to the #questions div:
         function createQuestion(questions) {
             for (var i = 0; i < questions.length; i++) {
                 $(".start").hide();
-                $("#questions").append('<form id="' + i + '" class="center-text"><p>Question ' + (i + 1) + ' of ' + questions.length + '</p><h3 class="question">' + questions[i].q + '</h3>' + radioButtons(questions[i].s, i) + '<button type="submit" class="next">NEXT &#8594;</button></p></form>');
+                $("#questions").append('<form id="' + i + '" class="center-text"><p>Question ' + (i + 1) + ' of ' + questions.length + '</p><h3 class="question">' + questions[i].q + '</h3>' + radioButtons(questions[i].s, i) + '<button type="submit" class="next">SUBMIT &#8594;</button></p></form>');
             }
             //This hides all except the first question:
             for (var k = questions.length - 1; k > 0; k--) {
@@ -87,28 +93,62 @@ $(document).ready(function () {
             });
         }
         
+
+        var scoretotal = 0
+
+        //logic that updates the score display
+        function updateScore(){
+            scoretotal = scoretotal + 100;
+            $( "#scorebox" ).val(scoretotal);
+            }             
+
+
+            //will highlight in green correct answer
+            function showCorrectAnswer(answer,userInput) { 
+
+                if(answer==userInput){
+
+                $("input[type='radio'][value='" + answer + "']").parent().css('color', '#00CE00');
+            }
+                else {
+
+                $("input[type='radio'][value='" + answer + "']").parent().addClass("correct");
+                $("input[type='radio'][value='" + userInput + "']").parent().addClass("incorrect");
+
+            }
+
+        }
+
+        
+
+
         //This checks the user's answer and updates the score:
         function checkAnswer(answer, qNum, questions) {
             if (answer == questions[qNum].a) {
                 questions[qNum].correct = 1;
                 scoreAry.push(questions[qNum].correct);
+                updateScore();
+            
+
             } else {
                 scoreAry.push(questions[qNum].correct);
+                
             }
+
+            var userInput = answer;
+
+            showCorrectAnswer(questions[qNum].a, answer);
+
+
         }
+
+
         
         createQuestion(questions);
-        
-        $(".next").click(function (event) {
-            event.preventDefault(); //This stops the form from submitting
-            var qNum = $(this).closest("form").attr("id"); //This gives us the question number
-            var userInput = $('input[name=' + qNum + ']:radio:checked').val(); //This grabs the user's selected answer
-            if (counter > 1) {
-                checkAnswer(userInput, qNum, questions);
-                $("#" + qNum).hide();
-                $("#" + qNum).next().show();
-                counter--;
-            } else if (counter == 1) {
+
+
+        function showSummary(userInput, qNum, questions) {
+
                 checkAnswer(userInput, qNum, questions);
                 $("#questions").find("form").remove();
                 $("#questions").append('<h3 class="result"></h3>');
@@ -119,6 +159,34 @@ $(document).ready(function () {
                             $("#questions").append('<p class="missed-' + j + '">You missed: ' + questions[j].q + ' ' + questions[j].a + '</p>');      
                         }
                     }
+        }
+
+
+
+        function showNextQuestion(qNum){
+                $("#" + qNum).hide();
+                $("#" + qNum).next().show();
+                counter--;
+        }
+
+
+
+        
+        $(".next").click(function (event) {
+            event.preventDefault(); //This stops the form from submitting
+            var qNum = $(this).closest("form").attr("id"); //This gives us the question number
+            var userInput = $('input[name=' + qNum + ']:radio:checked').val(); //This grabs the user's selected answer
+            if (counter > 1) {
+                checkAnswer(userInput, qNum, questions);
+                $(".next").attr('disabled',true);
+                setTimeout(function(){
+                    $(".correct").removeClass("correct");
+                    $(".incorrect").removeClass("incorrect");
+                    $(".next").removeAttr('disabled');
+                    showNextQuestion(qNum);
+                },3000);
+            } else if (counter == 1) {
+                 showSummary();
             } else {
                 return false;
             }
